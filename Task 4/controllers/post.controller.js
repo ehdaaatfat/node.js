76cc3b6b8471d0d37376fccce3f4f4b64bb1bdcb
ehdaaatfat <https://github.com/ehdaaatfat/Nti-Tasks.git>
2022-06-15 09:20:33 +0200

    task4

---
+++ Task 4/controllers/post.controller.js
@@ -0,0 +1,91 @@
+const deal = require("./dealwithJson.controller")
+class Post{
+    static home = (req, res)=> { 
+        const data = deal.readDataFromJSON("models/posts.json")
+        // res.send(data)
+        res.render("home", {
+            pageTitle:"all posts",
+            data,
+            isEmpty: !data.length
+        })
+    }
+    static add = (req, res)=> { 
+        if(req.query.title){
+            // const post = req.query
+            // post.id = Date.now()
+            const post = {...req.query, id: Date.now()}
+            const data = deal.readDataFromJSON("models/posts.json")
+            data.push(post)
+            deal.writeDataToJSON("models/posts.json", data)
+            res.redirect("/")
+        }
+        else{
+            res.render("add", {
+                pageTitle:"Add Post"
+            })    
+        }
+    }
+    static addPost = (req, res)=> { 
+        res.render("addpost", {
+            pageTitle:"Add Post"
+        })    
+    }
+    static addPostLogic = (req,res)=>{
+        const post = {...req.body, id: Date.now()}
+        const data = deal.readDataFromJSON("models/posts.json")
+        data.push(post)
+        deal.writeDataToJSON("models/posts.json", data)
+        res.redirect("/")    
+    }
+
+    static edit =  (req, res)=> { 
+        const postId = req.params.id
+        const data = deal.readDataFromJSON("models/posts.json")
+        const postData = data.find(p=> p.id == postId)
+        res.render("edit", {
+            pageTitle:"Edit Post",
+           postData
+        })
+        
+        }
+
+        static editPost = (req, res) => {
+            const postId = req.params.id;
+            const data = deal.readDataFromJSON("models/posts.json")
+            const index = data.findIndex((p) => p.id == postId);
+            console.log(data[index]);
+            data[index] = { ...req.body, id: postId };
+            deal.writeDataToJSON("models/posts.json", data);
+            res.redirect("/");
+          };
+        
+
+
+
+
+
+
+    static single = (req, res)=> { 
+        // res.send(req.params)
+        const postId = req.params.id
+        const data = deal.readDataFromJSON("models/posts.json")
+        const postData = data.find(p=> p.id == postId)
+        res.render("single", {
+            pageTitle:"single Post",
+            postData
+        })
+    }
+
+
+    /*static test = (req,res)=>{
+        res.render("test")
+    }*/
+    static delItem = (req,res)=>{
+        const postId = req.params.id
+        const data = deal.readDataFromJSON("models/posts.json")
+        const postData = data.filter(p=> p.id != postId)
+        deal.writeDataToJSON("models/posts.json", postData)
+        res.redirect("/")
+    }
+}
+module.exports = Post
---
+++ Task 4/routes/post.routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router()
+const postController = require("../controllers/post.controller")
+
+router.get("/", postController.home)
+
+router.get("/single/:id",postController.single)
+
+router.get("/add", postController.add)
+
+router.get("/addPost", postController.addPost)
+router.post("/addPost", postController.addPostLogic)
+
+router.get("/edit/:id",postController.edit)
+router.post("/edit/:id",postController.editPost)
+
+router.get("/delete/:id", postController.delItem)
+// router.get("/test", postController.test)
+// router.post("/test", (req,res)=> res.send("hello"))
+module.exports = router
