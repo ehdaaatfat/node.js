1cb05c2327092f8f54ecbfde5a3050398cb44c4a
ehdaaatfat <https://github.com/ehdaaatfat/Nti-Tasks.git>
2022-06-26 09:24:27 +0200

    angular task

---
+++ task-Angular/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; 
+
+import { AppComponent } from './app.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SliderComponent } from './components/slider/slider.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogsComponent,
+    NavbarComponent,
+    FooterComponent,
+    SliderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
